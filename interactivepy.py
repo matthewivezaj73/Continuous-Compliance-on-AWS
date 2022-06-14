2b851b9ecad96beb4104f660bbb45888642ad0d7
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-14 08:49:51 -0400

    Extended print statement.

--- interactivepy.py
+++ interactivepy.py
@@ -4,7 +4,7 @@
 #asking the user to enter their name.
 userName = input("Please enter your name:\t")
 #Printing out a message to the user.
-print("Hello, my name is, ")
+print("Hello, my name is, "+ userName)
 #Printing a message to the user.
 print("Ok, let's now print the numbers 1 - 10.")
 #Created for loop to print numbers 1 - 10.
