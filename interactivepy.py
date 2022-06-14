499fa76ac6da5bc51743a5afa28b6adba4cc9c5a
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-14 08:47:12 -0400

    entered a comment.

--- interactivepy.py
+++ interactivepy.py
@@ -1,7 +1,7 @@
 #Matthew Ivezaj
 #6/14/2022
 #Interactivepy.py
-
+#asking the user to enter their name.
 userName = input("Please enter your name:\t")
 #Printing out a message to the user.
 print("Hello, my name is, ")
