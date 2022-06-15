2d0370bdb1e30319a693f2bc6df5da5ef613f9b4
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-14 20:35:49 -0400

    Generating a random num.

--- guessing_game/functions/guessing_game.py
+++ guessing_game/functions/guessing_game.py
@@ -8,3 +8,4 @@ def guessing_game():
         This function chooses a number between 0 and 100.
     """
     #Assigning a variable to a random number Generator that generates a number inbetween 0 and 100.
+    my_random_num = rand.randint(0, 100)
