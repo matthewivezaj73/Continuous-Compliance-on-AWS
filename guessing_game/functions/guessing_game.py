e06476e2ff938afc9e300a39f0b61a268ade35fe
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-14 20:38:17 -0400

    Removed function.

--- guessing_game/functions/guessing_game.py
+++ guessing_game/functions/guessing_game.py
@@ -9,7 +9,3 @@ def guessing_game():
     """
     #Assigning a variable to a random number Generator that generates a number inbetween 0 and 100.
     my_random_num = rand.randint(0, 100)
-def grab_input(userInput):
-    """
-        A function that collects user input.
-    """
