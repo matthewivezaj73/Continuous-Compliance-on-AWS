8a9e3345a31d603fb0f7eaf59d8d58a356d9550d
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-14 21:08:32 -0400

    typecasting user_guess

--- guessing_game/guessing_game_main.py
+++ guessing_game/guessing_game_main.py
@@ -14,7 +14,7 @@ while not not_done:
     #Accepting the user's guess on what the random number could be.
     user_guess = input("")
     #Handling the case where the user correctly guessed the number.
-    if user_guess == my_rand:
+    if int(user_guess) == my_rand:
         #Printing out a congratulations message to the user.
         print("Congratulations, your guess was correct!"+ "\n\n")
         #Setting the not_done flag to true to escape the loop.
