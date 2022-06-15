8fc4a58c31355db90f8b1a1d1efbfec37360332e
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-14 20:37:10 -0400

    Moved comment.

--- guessing_game/functions/guessing_game.py
+++ guessing_game/functions/guessing_game.py
@@ -1,8 +1,8 @@
 #Matthew Ivezaj
 #6/14/2022
+#Guessing Game
 #Importing library.
 import random as rand
-#Guessing Game
 def guessing_game():
     """
         This function chooses a number between 0 and 100.
@@ -10,4 +10,3 @@ def guessing_game():
     #Assigning a variable to a random number Generator that generates a number inbetween 0 and 100.
     my_random_num = rand.randint(0, 100)
 def grab_input(userInput):
-    
