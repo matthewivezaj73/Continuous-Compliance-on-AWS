5c79650ef9dcaf54e413169b3c5137616f70dd07
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-15 07:31:12 -0400

    Passed a variable to the args.

--- Summing Numbers/functions/sum_function.py
+++ Summing Numbers/functions/sum_function.py
@@ -1,7 +1,7 @@
 #Matthew Ivezaj
 #6/15/2022
 #Summing Numbers
-def mysum():
+def mysum(myNum):
     """
         Creating a function that can take any number 
         of arguments and adds the numbers together.
