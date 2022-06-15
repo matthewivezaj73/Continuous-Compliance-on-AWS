b9b33d116639586df8a207a18ec676b0fc6e128f
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-14 20:34:49 -0400

    Added a comment.

--- guessing_game/functions/guessing_game.py
+++ guessing_game/functions/guessing_game.py
@@ -7,4 +7,4 @@ def guessing_game():
     """
         This function chooses a number between 0 and 100.
     """
-    #Generator a random number inbetween 0 and 100.
+    #Assigning a variable to a random number Generator that generates a number inbetween 0 and 100.
