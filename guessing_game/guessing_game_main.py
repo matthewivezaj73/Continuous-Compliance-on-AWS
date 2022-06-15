bbcd1d32d1f85c174c325667669b217132c00194
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-14 20:46:48 -0400

    Creating a random number

--- guessing_game/guessing_game_main.py
+++ guessing_game/guessing_game_main.py
@@ -6,6 +6,7 @@ from functions.guessing_game import guessing_game
 #Creating a flag.
 not_done = False
 #Creating a random number.
+my_rand = guessing_game()
 #Testing for user input.
 while not not_done:
     #Asking the user what number was generated.
