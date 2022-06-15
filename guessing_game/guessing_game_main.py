7b451870896747fa8678dce1c451c20e16620c0e
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-14 21:05:35 -0400

    Constructed base of case.

--- guessing_game/guessing_game_main.py
+++ guessing_game/guessing_game_main.py
@@ -28,6 +28,7 @@ while not not_done:
         #Telling the user their choice was wrong and to please try again.
         print(f"Sorry, but, \'" + user_guess + "\' was too high, please try again."+ "\n\n")
     #Handling the case where the user enters a letter.
+    elif user_guess.isalpha:
     #Handling the case where the user's input is completely off.
     else:
         #Telling the user their choice wasn't even close.
